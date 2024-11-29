839bafa092e963c6568b54a9042a971ddeacc980
joaovsj <joaosilva.jesus90@gmail.com>
2024-11-28 23:14:15 -0300

    :sparkles: feat: dashboard service created

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,14 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '@components/header/header.component';
 import { SidebarComponent } from '@components/sidebar/sidebar.component';
+
+// Chart
+import Chart from 'chart.js/auto';
+
+// MDBootstrap
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [SidebarComponent, HeaderComponent],
+  imports: [SidebarComponent, HeaderComponent, MdbFormsModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
-export class ReportsComponent {
+export class ReportsComponent implements OnInit { 
 
+  public chart: any
+
+  constructor(){ }
+
+  public ngOnInit(){ 
+
+  const labels = ["January", "February", "March", "April", "May", "Juny", "July"];
+  const barCanvasEle: any = document.getElementById('MyChart')
+  const barChart = new Chart(barCanvasEle.getContext('2d'), {
+    type: 'bar',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+          label: 'Sales',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+      }
+  });
+
+  }
+
+  private getBarChartData(){
+    
+  }
+
+  public search(){
+
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: "https://stockflow-api-production.up.railway.app/api"
+  // url: "https://stockflow-api-production.up.railway.app/api"
 };
